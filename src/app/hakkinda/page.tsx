cddc432f19698f2f1d74d29597a2915cc22c2e60
Frastoly <semihtrn483@gmail.com>
2025-05-13 01:32:20 +0300

    Navbar Hakkında linki düzeltildi

--- src/app/hakkinda/page.tsx
+++ src/app/hakkinda/page.tsx
@@ -2,7 +2,7 @@ export default function Hakkinda() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-800 text-white">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Hakkında</h1>
+        <h1 className="text-4xl font-bold mb-4">Geliştirici</h1>
         <p className="text-2xl">Semih Turan</p>
       </div>
     </main>
